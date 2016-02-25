1919de37951bd3038499b305fd515c0fdc5ca3f1
nao <cui.naomi@gmail.com>
2016-02-24 23:13:44 -0500

    Simple database testing

---
+++ routes/db.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+
+/* GET characters database */
+router.get('/', function(req, res, next) {
+    res.send("DATABASE_URL: " + process.env.DATABASE_URL);
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM char_table', function(err, result) {
+            done();
+            if (err) {
+                console.error(err);
+                res.send("Error " + err);
+            }
+            else {
+                response.render('pages/db', { results: result.rows });
+            }
+        });
+    });
+})
+
+module.exports = router;
